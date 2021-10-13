82e94b636bb3e10827ff031385dbd0de76f40545
yein-lee <agop0310@gmail.com>
2021-10-13 18:30:45 +0900

    list 관리 형식 해시맵으로 변경

--- src/List.java
+++ src/List.java
@@ -1,27 +1,20 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class List {
-    int coke;
-    int cider;
-    int tejava;
-    int pepper;
-    int cornTea;
-    int samdasu;
+    HashMap<String, Integer> count = new HashMap<String, Integer>();
+    HashMap<String, Integer> price = new HashMap<String, Integer>();
 
     public void start() throws IOException {
         BufferedReader reader = new BufferedReader(
-                new FileReader("d:\\file.txt")
+                new FileReader("file.txt")
         );
 
         String str;
         while ((str = reader.readLine()) != null) {
             String [] beverage = str.split(" ");
-            if(beverage[0]=="콜라"){
-                this.coke = Integer.parseInt(beverage[1]);
-            }
         }
         reader.close();
     }
