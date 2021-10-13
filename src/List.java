5ffe4026b36bd03c8a1bf0a1b69ec4c0025a147f
yein-lee <agop0310@gmail.com>
2021-10-13 18:35:15 +0900

    list 종료함수 추가

--- src/List.java
+++ src/List.java
@@ -7,7 +7,10 @@ public class List {
     HashMap<String, Integer> count = new HashMap<String, Integer>();
     HashMap<String, Integer> price = new HashMap<String, Integer>();
 
+
     public void start() throws IOException {
+        //프로그램 실행 후 최초 1회만 실행한다.
+        //프로그램 실행 중에는 메모리에서 가격과 개수를 관리한다.
         BufferedReader reader = new BufferedReader(
                 new FileReader("file.txt")
         );
@@ -17,5 +20,9 @@ public class List {
             String [] beverage = str.split(" ");
         }
         reader.close();
+    }
+
+    public void quit() throws IOException{
+        //프로그램 종료 전에 stockList.txt를 업데이트 한다.
     }
 }
