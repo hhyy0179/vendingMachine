26dcd0707b877458f10d877f531e8297301dbe20
yein-lee <agop0310@gmail.com>
2021-10-14 11:47:57 +0900

    gitignore

--- src/List.java
+++ src/List.java
@@ -7,7 +7,6 @@ public class List {
     HashMap<String, Integer> count = new HashMap<String, Integer>();
     HashMap<String, Integer> price = new HashMap<String, Integer>();
 
-
     public void start() throws IOException {
         //프로그램 실행 후 최초 1회만 실행한다.
         //프로그램 실행 중에는 메모리에서 가격과 개수를 관리한다.
