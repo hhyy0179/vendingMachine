68d968573d1643ba6d813b18f6153a494a810b8b
junoflow22 <hjh3485@naver.com>
2021-10-20 17:26:41 +0900

    change,quit

--- src/Change.java
+++ src/Change.java
@@ -1,3 +1,36 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import testman.Client;
+
 public class Change {
-    public void start(){}
+	
+	Scanner scan= new Scanner(System.in);
+	
+    public void start(Client c){
+    	//입력받는 부분
+        String scanStr;
+        scanStr = scan.nextLine();
+
+        //String 처리 부분(공백제거)
+        String [] userInput = scanStr.strip().split("");
+        ArrayList<String> rUserInput = new ArrayList<String>();
+        for(int i=0; i< userInput.length; i++){
+            if(userInput[i]!=" "){
+                rUserInput.add(userInput[i]);
+            }
+        }
+        if(rUserInput.size()!=1) {
+        	System.out.println("명령어 뒤에 인자가 없어야 합니다. 다시 입력해주세요.\n");
+        }
+        else {
+        	if(c.getCurrentAmount()==0) {
+        		System.out.println("반환할 돈이 없습니다.");
+        	}
+        	else if(c.getCurrentAmount()>0) {
+        		System.out.println("잔돈("+c.getCurrentAmount()+")원이 반환되었습니다./n");
+        		c.setCurrentAmount(0);
+        	}
+        }
+    }
 }
--- src/Client.java
+++ src/Client.java
@@ -2,6 +2,15 @@ public class Client {
     int [] moneyList = {0, 50000, 10000, 5000, 1000, 500, 100, 50, 10};
     int currentAmount; //현재 금액
 
+    
+    public int getCurrentAmount() {
+		return currentAmount;
+	}
+
+	public void setCurrentAmount(int currentAmount) {
+		this.currentAmount = currentAmount;
+	}
+	
     void calAmount(int i){
         this.currentAmount += moneyList[i];
     }
